03d1daff747e9399e109c8365c66b347605bd940
E. F. Haghish <haghish.e.f@gmail.com>
2022-08-11 09:35:40 +0200

    0.0.1

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 
 
 .onAttach <- function(lib, pkg) {
-  packageStartupMessage('Extreme Missing Data Imputation with Automated Machine Learning\nhttps://github.com/haghish/mlim\n')
+  packageStartupMessage('Missing Data Imputation with Automated Machine Learning\nhttps://github.com/haghish/mlim\n')
 }
