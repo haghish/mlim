26319e717d50e486e4a8ecda151f2051632cc70d
E. F. Haghish <haghish.e.f@gmail.com>
2022-08-20 23:11:11 +0200

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 
 
 .onAttach <- function(lib, pkg) {
-  packageStartupMessage('Multiple Imputation with Automated Machine Learning\nhttps://github.com/haghish/mlim\n\nNote1: Multiple imputation feature is in Beta version\nNote2: help proof-reading the package code on GitHub to make multiple imputation great again!')
+  packageStartupMessage('Multiple Imputation with Automated Machine Learning\nhttps://github.com/haghish/mlim\n\nNote1: Multiple imputation feature is in Beta version\nNote2: the package is under fast development. installing the developmental version from GitHub is recommended!')
 }
