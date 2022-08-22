6f3330e9a9bd5b226809563cade05a2aaab29220
E. F. Haghish <haghish.e.f@gmail.com>
2022-08-23 01:16:34 +0200

    0.0.7

---
+++ R/h2o.check.R
@@ -0,0 +1,39 @@
+#' @title server.check
+#' @description safely examines the connection status with h2o server
+#' @importFrom h2o h2o.clusterIsUp
+#' @author E. F. Haghish
+#' @return logical. if TRUE, proceed with the analysis
+#' @keywords Internal
+#' @noRd
+
+server.check <- function() {
+
+  up      <- FALSE
+  healthy <- FALSE
+
+  # check that the cluster is up
+  # ============================================================
+  for (i in 1:3) {
+    if (!up) up <- tryCatch(h2o.clusterIsUp(),
+                            error = function(cond){
+                              cat("trying to connect to JAVA server...\n");
+                              return(NULL)})
+    if (!up) Sys.sleep(1)
+  }
+  if (!up) stop("h2o server is down... perhaps heavy RAM consumption crashed the JAVA server?\nNOTE: save your imputation with the 'save' argument to continue \n      from where the imputation crashed")
+
+  # make sure the cluster is healthy
+  # ============================================================
+  for (i in 1:3) {
+    if (!healthy) healthy <- tryCatch(h2o.clusterStatus()$healthy,
+                                      error = function(cond){
+                                        cat("trying to connect to JAVA server...\n");
+                                        return(NULL)})
+    if (!healthy) Sys.sleep(1)
+  }
+  if (!healthy) stop("h2o server is down... perhaps heavy RAM consumption crashed the JAVA server?\nNOTE: save your imputation with the 'save' argument to continue \n      from where the imputation crashed")
+
+
+  return(healthy)
+}
+
