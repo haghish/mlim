5b70318863800e249438adf3647f1f3511b6c2c0
E. F. Haghish <haghish.e.f@gmail.com>
2022-09-04 02:46:11 +0200

    0.0.8

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 
 .onAttach <- function(lib, pkg) {
-  packageStartupMessage('Multiple Imputation with Automated Machine Learning\nhttps://github.com/haghish/mlim\n\nNote1: The package is in Beta version and receives frequent \n       updates. Installing the development version from GitHub \n       is recommended. Visit the GitHub repository to \n       download mlim handbook\nNote2: If you find a bug, post it on GitHub or e-mail it to \n       haghish@uio.no\n')
+  packageStartupMessage('Multiple Imputation with Automated Machine Learning\nhttps://github.com/haghish/mlim\n\nNote1: The package receives frequent updates. Installing \n       the development version from GitHub is recommended. \n       A free handbook is also available for download at the \n       GitHub repository.\nNote2: If you find a bug, post it on GitHub or e-mail it to \n       haghish@uio.no\n')
 }
